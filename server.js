f192cd34d1d1dd6f55a8c3151567857e97f4f44a
REVN <svldtrvn58@gmail.com>
2023-09-01 22:02:38 -0500

    added changes

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express')
 const app = express()
 const PORT = 3001
 
-app.use(express.static('public'))
-
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
