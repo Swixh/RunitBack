48f6c8a6d3551badf0e3532514165e80db93a424
REVN <svldtrvn58@gmail.com>
2023-09-01 21:35:17 -0500

    Adding Poop

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
+app.get('/api', (req, res) => {
+    res.json({user: 'gay'})
+})
+
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Listening on port ${PORT}`)
 })
